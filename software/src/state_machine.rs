692b63dea11e3fcae90088a8b0506e1ad9f74d12
yuvve <37124628+yuvve@users.noreply.github.com>
2025-03-05 19:38:58 +0100

    Add enum state machine

---
+++ software/src/state_machine.rs
@@ -0,0 +1,82 @@
+#[derive(Clone, Copy)]
+pub enum State {
+    Idle,
+    Alarm,
+    Settings(Settings),
+    BackupBattery,
+}
+
+#[derive(Clone, Copy)]
+pub enum Settings {
+    ClockHours(usize),
+    ClockMinutes(usize),
+    AlarmHours(usize),
+    AlarmMinutes(usize),
+}
+
+#[derive(Clone, Copy)]
+pub enum Event {
+    AlarmDetected,
+    Encoder(EncoderEvent),
+    ResetButton,
+    Timeout,
+    VBUSDisconnected,
+    VBUSConnected,
+}
+
+#[derive(Clone, Copy)]
+pub enum EncoderEvent {
+    Rotated(usize),
+    Pressed,
+}
+
+pub trait StateMachine {
+    fn next(&self, event: Event) -> State;
+}
+
+impl StateMachine for State {
+    fn next(&self, event: Event) -> State {
+        match self {
+            State::Idle => match event {
+                Event::Encoder(EncoderEvent::Pressed) => State::Settings(Settings::ClockHours),
+                Event::VBUSDisconnected => State::BackupBattery,
+                Event::AlarmDetected => State::Alarm,
+                _ => State::Idle,
+            }
+
+            State::Alarm => match event {
+                Event::Encoder(encoder_event) => {
+                    match encoder_event {
+                        EncoderEvent::Pressed => State::Idle,
+                        _ => State::Alarm,
+                    }
+                }
+                Event::Timeout => State::Idle,
+                _ => State::Alarm,
+            }
+
+            State::Settings(settings) => match event {
+                Event::Encoder(EncoderEvent::Rotated(steps)) => match settings {
+                    Settings::ClockHours => State::Settings(Settings::ClockHours(steps)),
+                    Settings::ClockMinutes => State::Settings(Settings::ClockMinutes(steps)),
+                    Settings::AlarmHours => State::Settings(Settings::AlarmHours(steps)),
+                    Settings::AlarmMinutes => State::Settings(Settings::AlarmMinutes(steps)),
+                }
+                Event::Encoder(EncoderEvent::Pressed) => match settings {
+                    Settings::ClockHours => State::Settings(Settings::ClockMinutes(0)),
+                    Settings::ClockMinutes => State::Settings(Settings::AlarmHours(0)),
+                    Settings::AlarmHours => State::Settings(Settings::AlarmMinutes(0)),
+                    Settings::AlarmMinutes => State::Idle,
+                }
+                Event::Timeout => State::Idle,
+                _ => State::Settings(*settings),
+            }
+
+            State::BackupBattery => match event {
+                Event::VBUSConnected => State::Idle,
+                _ => State::BackupBattery,
+            }
+
+        }
+    }
+}
