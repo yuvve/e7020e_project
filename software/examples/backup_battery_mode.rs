ac7b6fcc9f8a4108fc22a05061c4b32143956c9a
yuvve <37124628+yuvve@users.noreply.github.com>
2025-03-07 13:04:34 +0100

    Backup mode working

--- software/examples/backup_battery_mode.rs
+++ software/examples/backup_battery_mode.rs
@@ -20,6 +20,8 @@ const TIMER_HZ: u32 = 1000; // 1000 Hz (1 ms granularity)
 
 #[rtic::app(device = nrf52833_hal::pac, dispatchers= [TIMER0])]
 mod app {
+    use embedded_hal::digital::v2::StatefulOutputPin;
+
     use super::*;
 
     #[monotonic(binds = SysTick, default = true)]
@@ -27,14 +29,13 @@ mod app {
 
     #[shared]
     struct Shared {
-        comp: hal::comp::Comp,
     }
 
     #[local]
     struct Local {
         rtt_power: UpChannel,
         rtt_trace: UpChannel,
-        //comp: hal::lpcomp::LpComp,
+        comp: hal::lpcomp::LpComp,
         output: Pin<Output<PushPull>>,
     }
 
@@ -64,61 +65,62 @@ mod app {
         let port0 = hal::gpio::p0::Parts::new(cx.device.P0);
         let comp_pin = port0.p0_02.into_floating_input();
 
-        let comp = hal::comp::Comp::new(cx.device.COMP, &comp_pin);
-        comp.vref(hal::comp::VRef::Int1V2);
-        comp.power_mode(hal::comp::PowerMode::LowPower);
-        //comp.enable_interrupt(hal::comp::Transition::Up);
-        //comp.enable_interrupt(hal::comp::Transition::Down);
-        comp.enable_interrupt(hal::comp::Transition::Cross);
-        comp.hysteresis(false);
-        comp.enable();
-
-        //let comp = hal::lpcomp::LpComp::new(cx.device.LPCOMP, &comp_pin);
-        //comp.vref(hal::lpcomp::VRef::_4_8Vdd);
-        //comp.enable_interrupt(lpcomp::Transition::Cross);
+        //let comp = hal::comp::Comp::new(cx.device.COMP, &comp_pin);
+        //comp.vref(hal::comp::VRef::Vdd);
+        //comp.power_mode(hal::comp::PowerMode::LowPower);
+        //comp.hysteresis_threshold_down(90);
+        //comp.hysteresis_threshold_up(150);
+        //comp.enable_interrupt(hal::comp::Transition::Cross);
         //comp.enable();
+
+        let comp = hal::lpcomp::LpComp::new(cx.device.LPCOMP, &comp_pin);
+        comp.vref(hal::lpcomp::VRef::_4_8Vdd);
+        comp.enable_interrupt(hal::lpcomp::Transition::Cross);
+        comp.enable();
 
         let mut output: Pin<Output<PushPull>> = port0.p0_15.into_push_pull_output(Level::Low).degrade();
         output.set_high().ok();
         
-        (Shared {comp: comp}, Local {rtt_power: channels.up.0, rtt_trace: channels.up.1, output}, init::Monotonics(mono))
+        (Shared {}, Local {rtt_power: channels.up.0, rtt_trace: channels.up.1, output, comp}, init::Monotonics(mono))
     }
 
-    #[idle(local = [rtt_trace], shared = [comp])]
-    fn idle(mut cx: idle::Context) -> ! {
+    #[idle(local = [rtt_trace])]
+    fn idle(cx: idle::Context) -> ! {
         writeln!(cx.local.rtt_trace, "Entering idle!").ok();
         loop {
-            let comp_read = cx.shared.comp.lock(|c| c.read());
-            writeln!(cx.local.rtt_trace, "Comp {:?}", comp_read).ok();
             asm::wfi();
         }
     }
 
-    #[task(binds = COMP_LPCOMP, local = [rtt_power, output], shared = [comp], priority=5)]
-    fn comp_lcomp(mut cx: comp_lcomp::Context) {
-        let comp_read = cx.shared.comp.lock(|c| c.read());
+    #[task(binds = COMP_LPCOMP, local = [rtt_power, output, comp], priority=1)]
+    fn comp_lcomp(cx: comp_lcomp::Context) {
+        let comp = cx.local.comp;
+        let comp_read = comp.read();
+        
+        if cx.local.output.is_set_high().ok().unwrap() {
+            cx.local.output.set_low().ok();
+        } else {
+            cx.local.output.set_high().ok();
+        }
 
         match comp_read {
-            hal::comp::CompResult::Above => {writeln!(cx.local.rtt_power, "Above").ok();}
-            hal::comp::CompResult::Below => {writeln!(cx.local.rtt_power, "Below").ok();}
+            hal::lpcomp::CompResult::Above => {writeln!(cx.local.rtt_power, "Above").ok();}
+            hal::lpcomp::CompResult::Below => {writeln!(cx.local.rtt_power, "Below").ok();}
         }
         
-        let (is_up, is_down, is_cross) = cx.shared.comp.lock(|c| {
-            (c.is_up(), c.is_down(), c.is_cross())
-        });
 
-        if is_up {
+        if comp.is_up() {
             writeln!(cx.local.rtt_power, "Upward crossing").ok();
         }
 
-        if is_down {
+        if comp.is_down() {
             writeln!(cx.local.rtt_power, "Downward crossing").ok();
         }
 
-        if is_cross {
+        if comp.is_cross() {
             writeln!(cx.local.rtt_power, "Crossing").ok();
         }
 
-        cx.shared.comp.lock(|c| c.reset_events());
+        comp.reset_events();
     }
 }
